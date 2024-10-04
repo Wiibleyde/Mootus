c6786c916b4498d052a2179e68ffa54b8c6391e0
Wiibleyde <nathan@bonnell.fr>
2024-10-04 12:37:48 +0200

    Dynamic word list
    Signed-off-by: Wiibleyde <nathan@bonnell.fr>

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
 import Grid from 'src/components/Grid/Grid';
 import Keyboard from 'src/components/Keyboard/Keyboard';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import EndScreen from './components/EndScreen';
+import axios from 'axios';
 
-const WORD = 'NATHAN';
+
 export const maxAttempts = 6;
 
 export enum LetterState {
-	Good = 'V',
-	Wrong = 'X',
-	WrongPlace = '_'
+	Good = 'G',
+	Wrong = 'W',
+	WrongPlace = 'N'
 }
 
 export enum GameState {
@@ -19,47 +20,72 @@ export enum GameState {
 }
 
 const App = () => {
-	const [attempts, setAttempts] = useState([WORD.substring(0, 1)]);
+	const [word, setWord] = useState<string>('');
+	const [attempts, setAttempts] = useState<Array<string>>([word.substring(0, 1)]);
 	const [results, setResults] = useState<string[]>([]);
-	const [gameState, setGameState] = useState(GameState.Playing);
+	const [gameState, setGameState] = useState<GameState>(GameState.Playing);
+	const [needRefindWord, setNeedRefindWord] = useState<boolean>(true);
+	const [isLoading, setIsLoading] = useState<boolean>(true);
+
+	const resetGame = () => {
+		setNeedRefindWord(true);
+		setIsLoading(true);
+		setResults([]);
+		// setBlockFirstLetter(false);
+	}
+
+	useEffect(() => {
+		if (needRefindWord) {
+			axios.get('https://trouve-mot.fr/api/random').then((response) => {
+				setWord(response.data[0].name.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase());
+				setAttempts([response.data[0].name.normalize("NFD").replace(/[\u0300-\u036f]/g, "").toUpperCase().substring(0, 1)])
+				setNeedRefindWord(false);
+				setIsLoading(false)
+				setGameState(GameState.Playing);
+			});
+		}
+	}, [needRefindWord]);
 
 	const handleKeyPress = (keyElement: string) => {
+		if(attempts[0].length === 1 && keyElement === '⌫') {
+			return;
+		}
+		if (attempts[0].length === 1 && keyElement === attempts[0]) {
+			return;
+		}
 		const copy = [...attempts];
 		switch (keyElement) {
 			case '⌫':
 				copy[attempts.length - 1] = copy[attempts.length - 1].slice(0, -1);
 				break;
 			case '⏎': {
-				if (copy[attempts.length - 1].length < WORD.length) {
-					console.log("Le mot n'est pas assez long");
+				if (copy[attempts.length - 1].length < word.length) {
 					return;
 				}
 				const attemptResult = copy[attempts.length - 1].split('').map((letter, index) => {
-					if (letter === WORD[index]) {
+					if (letter === word[index]) {
 						return LetterState.Good;
 					}
-					if (WORD.includes(letter) && letter !== WORD[index]) {
+					if (word.includes(letter) && letter !== word[index]) {
 						return LetterState.WrongPlace;
 					}
 					return LetterState.Wrong;
 				});
 				setResults([...results, attemptResult.join('')]);
-				if (copy[attempts.length - 1] === WORD) {
-					console.log('Win');
+				if (copy[attempts.length - 1] === word) {
 					setGameState(GameState.Win);
 					return;
 				}
 				if (attempts.length === maxAttempts) {
-					console.log('Lose');
 					setGameState(GameState.Lose);
 					return;
 				}
-				setAttempts([...attempts, WORD.substring(0, 1)]);
+				setAttempts([...attempts, word.substring(0, 1)]);
 				setGameState(GameState.Playing);
 				return;
 			}
 			default:
-				if (copy[attempts.length - 1].length === WORD.length) {
+				if (copy[attempts.length - 1].length === word.length) {
 					return false;
 				}
 				copy[attempts.length - 1] = `${copy[attempts.length - 1]}${keyElement}`;
@@ -68,16 +94,38 @@ const App = () => {
 		setAttempts(copy);
 	};
 
+	useEffect(() => {
+		function handleKeyDown(event: KeyboardEvent) {
+			const keyElement = event.key.toUpperCase();
+			if ((keyElement.match(/[A-Z]/) || keyElement === 'BACKSPACE' || keyElement === 'ENTER') && !(event.ctrlKey || event.altKey || event.metaKey || event.shiftKey || keyElement === 'TAB')) {
+				handleKeyPress(keyElement === 'BACKSPACE' ? '⌫' : keyElement === 'ENTER' ? '⏎' : keyElement);
+			}
+		}
+		window.addEventListener('keydown', handleKeyDown);
+		return () => {
+			window.removeEventListener('keydown', handleKeyDown);
+		};
+	}, [attempts, results]);
+
 	return (
 		<div className="bg-gray-100 dark:bg-black h-screen flex flex-col justify-center items-center">
 			<h1 className="text-center text-5xl mt-12 mb-8 font-bold dark:text-white">Mootus</h1>
 			{gameState === GameState.Playing && (
 				<div>
-					<Grid length={WORD.length} attempts={attempts} results={results} />
-					<Keyboard handleKeyPress={handleKeyPress} />
+					{!isLoading && (
+						<div>
+							<Grid length={word.length} attempts={attempts} results={results} />
+							<Keyboard handleKeyPress={handleKeyPress} />
+						</div>
+					)}
+					{isLoading && (
+						<div className="text-center">
+							<p className="text-lg dark:text-white">Chargement...</p>
+						</div>
+					)}
 				</div>
 			)}
-			<EndScreen gameState={gameState} />
+			<EndScreen gameState={gameState} handleReset={resetGame} word={word} />
 		</div>
 	);
 };
--- src/components/EndScreen.tsx
+++ src/components/EndScreen.tsx
@@ -3,22 +3,26 @@ import { GameState } from "src/App";
 
 interface Props {
     gameState: GameState;
+    handleReset: () => void;
+    word: string;
 }
 
-const EndScreen: React.FC<Props> = ({ gameState }) => {
+const EndScreen: React.FC<Props> = ({ gameState, handleReset, word }) => {
     switch (gameState) {
         case GameState.Win:
             return (
-                <div>
+                <div className="text-center">
 					<h2 className="text-2xl font-bold text-green-600">Gagné !</h2>
-                    <RestartButton />
+                    <p className="text-lg dark:text-white">Le mot était : {word}</p>
+                    <RestartButton handleReset={handleReset} />
 				</div>
             )
         case GameState.Lose:
             return (
-                <div>
+                <div className="text-center">
 					<h2 className="text-2xl font-bold text-red-600">Perdu !</h2>
-					<RestartButton />
+                    <p className="text-lg dark:text-white">Le mot était : {word}</p>
+					<RestartButton handleReset={handleReset} />
 				</div>
             )
         default:
@@ -26,6 +30,10 @@ const EndScreen: React.FC<Props> = ({ gameState }) => {
     }
 };
 
-const RestartButton: React.FC = () => <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={() => window.location.reload()}>Rejouer</button>;
+interface RestartButtonProps {
+    handleReset: () => void;
+}
+
+const RestartButton: React.FC<RestartButtonProps> = ({ handleReset }) => <button className="mt-4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={handleReset}>Recommencer</button>;
 
 export default EndScreen;
